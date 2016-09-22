e0408ddbbcc3c782210dfe6dd6bdefec685fbd8b
Michael Hanks <mikeHanks@Michaels-MacBook-Pro.local>
2016-09-22 13:14:52 -0600

    added drag and color functionality

--- app.js
+++ app.js
@@ -7,6 +7,39 @@ $(document).ready(function() {
   var yellow = $('#yellow')
   var red = $('#red')
 
+//to drag and drop:  while mouse down, if mouse enter
+//change class to color
+//until mouse up
+
+$(function() {
+  var isMouseDown = false,
+  isColored;
+  $('.box').mousedown(function() {
+    isMouseDown = true;
+    $(this).addClass(color);
+    isColored =
+    $(this).hasClass(color);
+      return false;
+  })
+  .mouseover(function() {
+    if(isMouseDown) {
+      $(this).addClass(color);
+    }
+  })
+  .bind("selectstart",function() {
+    return false;
+  })
+  $(document)
+    .mouseup(function() {
+      isMouseDown = false;
+    })
+})
+
+  // $('.box').on('mousedown', function() {
+  //   $(this).on('mouseenter'), function() {
+  //     $(this).addClass(colors)
+  //   }
+  // })
 
 
   $('.box').on('click', function() {
